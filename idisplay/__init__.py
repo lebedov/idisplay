d51346e2bfb99b6cec755ecc1ae97b95d119e071
Lev Givon <lev@columbia.edu>
2014-10-02 18:54:42 -0400

    Bug fixes, add alias.

--- idisplay/__init__.py
+++ idisplay/__init__.py
@@ -5,9 +5,10 @@ from IPython.core.magic import Magics, magics_class, line_magic, cell_magic
 
 @magics_class
 class DisplayMagic(Magics):
+
     @line_magic
     @cell_magic
-    def display(self, line, cell):
+    def display(self, line, cell=''):
         """
         Display something using IPython's rich display system.
 
@@ -20,27 +21,50 @@ class DisplayMagic(Magics):
         -m, --math    : load LaTeX math expression
         -s, --svg     : load SVG file
         -y, --youtube : load YouTube ID
+
+        Examples
+        --------
+        %display -i myimage.png
+
+        %display -m '\Delta x + y^3'
+
+        %%display -h \"\"\"
+        <ul>
+        <li>This</li>
+        <li>is</li>
+        <li>a list.</li>
+        </ul>
+        \"\"\"
+
+        Notes
+        -----
+        %disp is automatically aliased to %display.
         """
         
         opts, cell = self.parse_options('%s\n%s' % (line, cell),
                                         'h:i:j:l:m:s:y:',
                                         ['image=', 'html=', 'json=', 'latex=',
                                          'math=', 'svg=', 'youtube='])
-        for opt, arg in opts:
-            if opt in ['h', '--html']:
-                HTML(arg)
-            elif opt in ['-i', '--image']:
-                Image(arg)
-            elif opt in ['-j', '--json']:
-                JSON(arg)
-            elif opt in ['-l', '--latex']:
-                Latex(arg)
-            elif opt in ['-m', '--math']:
-                Math(arg)
-            elif opt in ['-s', '--svg']:
-                YouTubeVideo(arg)
-            elif:
-                raise ValueError('unrecognized option')
+        for opt, arg in opts.iteritems():
+            if opt in ['h', 'html']:
+                return HTML(arg)
+            elif opt in ['i', 'image']:
+                return Image(arg)
+            elif opt in ['j', 'json']:
+                return JSON(arg)
+            elif opt in ['l', 'latex']:
+                return Latex(arg)
+            elif opt in ['m', 'math']:
+                return Math(arg)
+            elif opt in ['s', u's', 'svg']:
+                return SVG(arg)
+            elif opt in ['y', 'youtube']:
+                return YouTubeVideo(arg)
+
+        # Raise an exception if no options were specified:
+        raise ValueError('Format: [option] <file|URI>')
 
 def load_ipython_extension(ip):
     ip.register_magics(DisplayMagic)
+    ip.magics_manager.register_alias('disp', 'display', 'line')
+    ip.magics_manager.register_alias('disp', 'display', 'cell')
